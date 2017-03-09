c16e9c099c593d16d1fa1d261e5f0729a53b6ad4
TripleHwang <hwanghhh82@gmail.com>
2017-03-09 13:06:46 +0900

    2nd Commit

--- Exercise1.c
+++ Exercise1.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <conio.h>
 
+void intro(){
+	printf("2016004020 황한혁입니다 게임을 좋아합니다.")
+}
+
 void main(){
 	char a;
 	printf("type 1!");
 	a = getch();
 	if(a=='1'){
 		printf("Hwang Hanhyeok!");
+	}
+	else if(a=='2'){
+		intro();
 	}
 }
